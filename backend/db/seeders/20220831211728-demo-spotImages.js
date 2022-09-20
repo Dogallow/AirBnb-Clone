72ae1aaf187a835fc8b866867b36300f5bd776b6
Donovan Galloway <genericsalesman6@gmail.com>
2022-09-20 15:17:35 -0700

    A oversized chunk of functionality in the spots feature. CRUD should be complete or nearly complete for spots

--- backend/db/seeders/20220831211728-demo-spotImages.js
+++ backend/db/seeders/20220831211728-demo-spotImages.js
@@ -14,17 +14,17 @@ module.exports = {
    await queryInterface.bulkInsert('SpotImages', [
     {
       spotId: 1,
-      url: 'www.google.com',
+       url: 'https://images.unsplash.com/photo-1534371020656-6b85825f2b1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dHJhbnN5bHZhbmlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       preview:true
     },
     {
       spotId: 2,
-      url: 'www.shakeshack.com',
+      url: 'https://images.unsplash.com/photo-1536125434175-6c5657605fb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGlnaHRob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
       preview:true
     },
     {
       spotId: 1,
-      url: 'www.youtube.com',
+      url: 'https://images.unsplash.com/photo-1605513512381-74b4f54380e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJhbnN5bHZhbmlhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       preview:true
     },
    ])
