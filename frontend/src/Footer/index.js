dcf2e6831f5ced4de63b2bfe38677d3d4d658eb1
Donovan Galloway <genericsalesman6@gmail.com>
2022-10-21 11:24:45 -0700

    Border added

--- frontend/src/Footer/index.js
+++ frontend/src/Footer/index.js
@@ -2,7 +2,7 @@
 
 const Footer = () => {
     return (
-        <div style={{width: '100%',display:'flex', justifyContent: 'right'}}>
+        <div style={{width: '100%',display:'flex', justifyContent: 'right', alignItems: 'center', borderTop:' 1px solid #DDD'}}>
             Created by Donovan Galloway:
             <a href="https://github.com/Dogallow" target="_blank">
                 <img style={{height: '20px', width: '20px', marginLeft: '5px'}} src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png" alt="Github Logo" />
