a1cdcea4a0ae02a41ebd8d5b5df066646cd57cd6
Donovan Galloway <genericsalesman6@gmail.com>
2022-10-21 11:18:33 -0700

    About link added

---
+++ frontend/src/Footer/index.js
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+    return (
+        <div style={{width: '100%',display:'flex', justifyContent: 'right'}}>
+            Created by Donovan Galloway:
+            <a href="https://github.com/Dogallow" target="_blank">
+                <img style={{height: '20px', width: '20px', marginLeft: '5px'}} src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png" alt="Github Logo" />
+            </a>
+        </div>
+    )
+}
+
+export default Footer
